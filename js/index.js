eb25c559733cad6100df92f044e618bd23877a4a
marie-hotchkiss <mtknowlton67@gmail.com>
2020-01-17 15:19:26 -0800

    Added mouseOver

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// Mouse Over //
+ 
+const myButton1 = document.getElementsByClassName('btn')[0];
+ 
+//console.log(myButton1);
+ 
+myButton1.addEventListener('mouseover', function (e) {
+ 
+myButton1.style.color = 'black';
+ 
+myButton1.style.backgroundColor = 'yellow';
+ 
+});
+ 
+ 
+
