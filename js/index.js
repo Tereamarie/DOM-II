0c8be8b5413136beafd889756779c0610135a1b8
marie-hotchkiss <mtknowlton67@gmail.com>
2020-01-17 15:33:23 -0800

    Added Double click/scroll event

--- js/index.js
+++ js/index.js
@@ -14,5 +14,28 @@ myButton1.style.backgroundColor = 'yellow';
  
 });
  
- 
+ //Double Click//
 
+
+const myButton2 = document.getElementsByClassName('btn')[1];
+//console.log(myButton2);
+myButton2.addEventListener('dblclick', function(e) {
+  myButton2.style.color = 'yellow';
+  myButton2.style.backgroundColor = 'black';
+});
+
+// Click //
+
+const myButton3 = document.getElementsByClassName('btn')[2];
+
+myButton3.addEventListener('click', function(e) {
+  myButton3.style.color = 'blue';
+  myButton3.style.backgroundColor = 'green';
+});
+
+
+//Scroll//
+
+function randomColors() {
+  const letters = ''
+}
